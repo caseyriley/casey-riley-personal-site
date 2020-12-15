5b352291e1d629f5c2ada9b5c0005f86afd09db8
Casey <innerforest7@gmail.com>
2020-12-15 00:44:14 -0800

    add contact page

---
+++ dist/banner-orb.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  const bannerC = document.getElementById("banner-c");
+  const ring = document.getElementById("ring");
+  const colorWash = document.getElementById("color-wash");
+  const colorWash2 = document.getElementById("color-wash2");
+ 
+
+  let colorState = false;
+
+  function toggleColorState(){
+  
+    colorState = !colorState;
+ 
+    bannerC.classList.add(`${colorState ? "dim" : "lit"}`)
+    bannerC.classList.remove(`${colorState ? "lit" : "dim"}`)
+    ring.classList.add(`${colorState ? "ring-on" : "ring-off"}`)
+    ring.classList.remove(`${colorState ? "ring-off" : "ring-on"}`)
+    colorWash.classList.add(`${colorState ? "color-lit" : "color-dim"}`)
+    colorWash.classList.remove(`${colorState ? "color-dim" : "color-lit"}`)
+    colorWash2.classList.add(`${colorState ? "color-lit" : "color-dim"}`)
+    colorWash2.classList.remove(`${colorState ? "color-dim" : "color-lit"}`)
+  }
+
+  bannerC.onmouseenter = function() {toggleColorState()};
+  
+})
